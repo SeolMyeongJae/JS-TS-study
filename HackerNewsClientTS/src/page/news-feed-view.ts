bf04ede0b3ce9586746307500409bfea7734d5d0
Myeongjae <tjfaudwo0818@gmail.com>
2021-11-08 21:06:44 +0900

    ch4-9 store 클래스 작성

--- HackerNewsClientTS/src/page/news-feed-view.ts
+++ HackerNewsClientTS/src/page/news-feed-view.ts
@@ -1,6 +1,6 @@
 import View from '../core/view';
 import { NewsFeedApi } from '../core/api';
-import { NewsFeed } from '../types';
+import { NewsStore } from '../types';
 import { NEWS_URL } from '../config';
 
 const template: string = `
@@ -30,28 +30,28 @@ const template: string = `
 
 export default class NewsFeedView extends View {
   private api: NewsFeedApi;
-  private feeds: NewsFeed[];
+  private store: NewsStore;
 
-  constructor(containerId: string) {
+  constructor(containerId: string, store: NewsStore) {
     super(containerId, template);
 
     this.api = new NewsFeedApi(NEWS_URL);
-    this.feeds = window.store.feeds;
+    this.store = store;
 
-    if (this.feeds.length === 0) {
-      this.feeds = window.store.feeds = this.api.getData();
-      this.makeFeeds();
+    if (!this.store.hasFeeds) {
+      this.store.setFeeds(this.api.getData());
     }
   }
 
-  render(): void {
-    window.store.currentPage = Number(location.hash.substr(7) || 1);
-    for (let i = (window.store.currentPage - 1) * 10; i < window.store.currentPage * 10; i++) {
-      const { id, title, comments_count, user, points, time_ago, read } = this.feeds[i];
+  render(page: string = '1'): void {
+    this.store.currentPage = Number(page);
+
+    for (let i = (this.store.currentPage - 1) * 10; i < this.store.currentPage * 10; i++) {
+      const { id, title, comments_count, user, points, time_ago, read } = this.store.getFeed(i);
       this.addHtml(`
         <div class="p-6 ${
-          this.feeds[i].read ? 'bg-red-500' : 'bg-white'
-        } mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
+          read ? 'bg-red-500' : 'bg-white'
+        } mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100"> 
         <div class="flex">
           <div class="flex-auto">
             <a href="#/show/${id}">${title}</a>  
@@ -61,7 +61,7 @@ export default class NewsFeedView extends View {
           </div>
         </div>
         <div class="flex mt-3">
-          <div class="grid grid-cols-3 text-sm text-gray-500">
+          <div class="grid grid-cols-3 text-sm text-gray-500"> 
             <div><i class="fas fa-user mr-1"></i>${user}</div>
             <div><i class="fas fa-heart mr-1"></i>${points}</div>
             <div><i class="far fa-clock mr-1"></i>${time_ago}</div>
@@ -72,20 +72,9 @@ export default class NewsFeedView extends View {
     }
 
     this.setTemplateData('news_feed', this.getHtml());
-    this.setTemplateData('prev_page', window.store.currentPage > 1 ? String(window.store.currentPage - 1) : String(1));
-    this.setTemplateData(
-      'next_page',
-      this.feeds.length / 10 > window.store.currentPage
-        ? String(window.store.currentPage + 1)
-        : String(window.store.currentPage)
-    );
+    this.setTemplateData('prev_page', String(this.store.prevPage));
+    this.setTemplateData('next_page', String(this.store.nextPage));
 
     this.updateView();
-  }
-
-  private makeFeeds(): void {
-    for (let i = 0; i < this.feeds.length; i++) {
-      this.feeds[i].read = false;
-    }
   }
 }
